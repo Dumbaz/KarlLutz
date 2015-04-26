1e24a58017a669f20ecda314740d4cc6242420bd
Dorian <dorian.lenzner@mailbox.tu-dresden.de>
2015-04-26 18:26:35 +0200

    Push for remote

--- Dorian/etree.py
+++ Dorian/etree.py
@@ -1,11 +1,36 @@
 from lxml import etree
 
 
-tree = etree.parse('../speyer-192-20-hackathon.xml')
+tree = etree.parse('../192-20.xml')
 
-cpackages = tree.xpath('/archdesc/dsc/c/')
+root = tree.getroot()
 
-print cpackages
+for child in root:
+	print child.tag
+
+r = root.xpath('//archdesc/dsc/c/c')
+t = r[0].xpath('did/unittitle/text()')
+
+# for x in xrange(0,165):
+	# print r[x].xpath('did/unittitle/text()')
+
+# print r
+
+id = r[0].xpath('@id')
+unitdate = r[0].xpath('did/unitdate/text()')
+unittitle = r[0].xpath('did/unittitle/text()')
+daoloc = r[0].xpath('daogrp[1]/daoloc[@xlink:role="image_full"]/href')
+
+print id
+print unitdate
+print unittitle
+print daoloc
+
+
+# print root.find('{urn:isbn:1-931666-22-9}archdesc')
+
+
+
 
 # for cpackage in root.findall("c level="file"")
 # 	print cpackage
